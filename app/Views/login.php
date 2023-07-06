7beb84e41180bca64d2bc34d648e6f1a4ad09a1f
toneswari <toneswarinandam037@gmail.com>
2023-07-06 16:19:31 +0530

    changed login and register files

--- app/Views/login.php
+++ app/Views/login.php
@@ -3,39 +3,99 @@
 <html>  
 <head>  
     <title>Login</title>  
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>  
 
 <style>
-    input[type=text] {
-width:40%;
-padding: 12px 20px;
+html {   
+    height: 100%;   
+}  
+body {   
+    height: 100%;   
+}  
+.global-container {  
+    height: 100%;  
+    display: flex;  
+    align-items: center;  
+    justify-content: center;  
+    background-color: #f5f5f5;  
+}  
+form {  
+    padding-top: 10px;  
+    font-size: 14px;  
+    margin-top: 30px;  
+}  
+.card-title {   
+font-weight: 300;  
+ }  
+.btn {  
+    font-size: 14px;  
+    margin-top: 20px;  
+}  
+.login-form {   
+    width: 330px;  
+    margin: 20px;  
+}  
+.sign-up {  
+    text-align: center;  
+    padding: 20px 0 0;  
+}  
+.alert {  
+    margin-bottom: -30px;  
+    font-size: 13px;  
+    margin-top: 20px;  
+} 
+.btn{
+    border-radius:10px;
+}
+h3{
+    font-weight: bold;
+} 
+p{
+    text-align:center;
+}
+container{
+    text-align:center;
+    align-items:center;
+    justify-content:center;
+}
 
-
-}
-input[type=password] {
-width:40%;
-padding: 12px 20px;
-}
-input[type=button], input[type=submit], input[type=reset] {
-background-color: #04AA6D;
-border: none;
-color: white;
-padding: 16px 32px;
-text-decoration: none;
-margin: 4px 2px;
-cursor: pointer;
-width:40%;
-}
     </style>
-<body> <h1 style="text-align:center;color:green;padding-top:40px;">Login</h1> 
-<form style="text-align:center;color:green;padding-top:40px;" action="<?= base_url('login'); ?>" method="post">
-Username<label for="username">
-    <input type="text" name="username" id="username" placeholder="enter username"><br><br>
-    Password<label for="password">
-    <input type="password" name="password" id="password" placeholder="enter password"><br><br>
-    <input type="submit" value="submit" class="btn btn-block">
-</form>   
+<body> 
+<div class="pt-5">  
+  <div class="global-container">  
+    <div class="card login-form">  
+    <div class="card-body">  
+        <h3 class="card-title text-center"> Login into your account </h3>  
+        <p>Enter your registered Mobile or Email ID</p>
+        <div class="card-text">  
+            <form>  
+                <div class="form-group">   
+                    <input type="email" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Mobile or Email ID">  
+                </div>  
+                <div class="form-group">  
+                    <a href="#" style="float:right;font-size:12px;"> Forgot password? </a>  
+                    <input type="password" class="form-control form-control-sm" id="exampleInputPassword1" placeholder="Enter Password">  
+                </div>  
+                <button type="submit" class="btn btn-primary btn-block"> Continue </button>  
+                <hr>
+                <button type="submit" class="btn btn-outline-primary btn-block"> Login with OTP </button> 
+                <hr> 
+                <p>or Login using social media</p><br>
+                <div class="container" >
+                <img src="<?=base_url()?>public/images/google.png" height=50px width=50px>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <i class="fa fa-facebook-official" style="color:blue;font-size:48px;height:50px;width:50px;"></i>
+</div>
+                <div class="sign-up">  
+                    Don't have an account? <a href="#"> Register Here </a>  
+                </div>  
+            </form>  
+        </div>  
+    </div>  
+</div>  
+</div>     
 </body>  
 </html>  
 
--- app/Views/register.php
+++ app/Views/register.php
@@ -1,76 +1,173 @@
 <html>
     <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <style>
-.container-fluid{
-padding-top:40px; 
-
-    /* padding:5pt 5pt 5pt 5pt;
-    width:357pt;
-    height:547pt;
-    transform: translate(17pt, 143pt);
-    color:#FFFF;
-    size:1pt;
-    border:#3333331A;
-    opacity:100%; */
-}
-input[type="text"]{
-    width:30%;
-padding: 12px 20px;
+body {
+  background-color: white;
+  display:flex;
+  height:100vh;
+  justify-content:center;
+  align-items:center;
+  background-size:cover;
+  flex-direction:column;
 }
 
-input[type=password],input[type="email"] {
-width:30%;
-padding: 12px 20px;
+.container{
+    width:100%;
+    max-width:650px; 
+    padding:28px;
+    margin:0 28px;
+    border-radius:10px;
+    box-shadow:inset -2px 2px 2px white;
 }
-button {
-background-color: #04AA6D;
-border: none;
-color: white;
-padding: 16px 32px;
-text-decoration: none;
-margin: 4px 2px;
-cursor: pointer;
-width:30%;
+.form-title{
+    font-size:26px;
+    font-weight:600;
+    text-align:center;
+    padding-bottom:6px;
+    /* text-shadow:2px 2px 2px black; */
+    border-bottom:solid 1px white;
+    color:black;
+
 }
-/* h4{
-color:black;
-width:86pt;
-height:22pt;
-transform: translate(151pt, 167pt);
-size:18pt;
+.main-user-info{
+    display:flex;
+    flex-wrap:wrap;
+    justify-content:space-between;
+    padding:20px 0;
+
+}
+
+.user-input-box:nth-child(2n){
+justify-content:end;
+}
+
+.user-input-box{
+    display:flex;
+    flex-wrap:wrap;
+    width:50%;
+    padding-bottom:15px;
+}
+
+.user-input-box input{
+    height:40px;
+    width:95%;
+    border-radius:7px;
+    outline:none;
+    border:1px solid grey;
+    padding:0 10px;
+    padding-left:10px;
+
+}
+.form-submit-btn button{
+cursor:pointer;
+}
+
+.form-submit-btn{
+    margin-top:40px;
+}
+
+
+.form-submit-btn button{
+display:block;
+width:100%;
+margin-top:10px;
+font-size:20px;
+padding:10px;
+border:none;
+border-radius:3px;
+}
+*{
+    padding:0;
+    margin:0;
+    box-sizing:border-box;
+    font-family:san-serif;
+}
+
+form{
+display:grid;
+place-content:center;
+grid-template-columns:100%;
 text-align:center;
-line-height: 22;
-font-style:Rubik;
-} */
+}
+
+
+
+form{
+    flex-grow:1;
+}
+
+/* || small */
+@media (max-width: 1200px) {
+.container{
+    min-width:280px;
+}
+
+
+
+  .user-input-box{
+    margin-bottom:12px;
+    width:100%;
+  }
+  .user-input-box:nth-child(2n){
+    justify-content:space-between;
+  }
+  .main-user-info{
+    max-height:380px;
+    overflow:auto;
+  }
+  .main-user-info::-webkit-scrollbar{
+    width:0;
+  }
+}
+
+
+
+
 
 
 </style>
+</head>
+
+
 <body style="text-align:center;">
 
 
-<!-- <br><br> -->
 
-    <!-- <div class="card">
-    <div class="card-body">
-    <img src="<?=base_url()?>public/images/register.png" alt="first">   
-</div> -->
+<div class="container">
 
-<div class="container-fluid">
-<fieldset>
-    <h4 style="color:black;">Register</h4><br><br>
-    <input type="text" name="first name" id="first" placeholder="First Name"><br><br>
-    <input type="text" name="last name" id="last" placeholder="Last Name"><br><br>
-    <input type="text" name="phone number" id="phone" placeholder="Mobile Number"><br><br>
-    <input type="email" name="Email" id="email" placeholder="Email ID"><br><br>
-    <input type="password" name="password" id="pass" placeholder="Password"><br><br>
-    <input type="password" name="confirm password" id="confirm" placeholder="Confirm Password"><br><br>
-    <button class="btn  btn-primary btn-md">Continue</button><br><br>
+    <h1 class="form-title">Register</h1><br><br>
+    <form action="#">
+        <div class="main-user-info">
+            <div class="main-input-box">
+                
+    <input type="text" name="first name" id="first" placeholder="First Name">
+</div><br><br>
+<div class="main-input-box">
+    <input type="text" name="last name" id="last" placeholder="Last Name">
+</div><br><br>
+<div class="main-input-box">
+    <input type="text" name="phone number" id="phone" placeholder="Mobile Number">
+</div><br><br>
+<div class="main-input-box">
+    <input type="email" name="Email" id="email" placeholder="Email ID">
+</div><br><br>
+<div class="main-input-box">
+    <input type="password" name="password" id="pass" placeholder="Password">
+</div><br><br>
+<div class="main-input-box">
+    <input type="password" name="confirm password" id="confirm" placeholder="Confirm Password">
+</div><br><br>
+<div class="form-submit-btn">
+    <button class="btn  btn-primary btn-block">Continue</button><br><br>
+</div><br><br>
     <a href="#" style="text-decoration:none;">Already have an account?</a><br><br>
     <a href="#">Login Here</a>
-
-    </fieldset>
+</div>
+</form>
 </div>
 
 
